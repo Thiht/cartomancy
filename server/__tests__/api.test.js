e769f23fa8d004156fdab51124f40ae433368796
Thibaut Rousseau <thibaut.rousseau@protonmail.com>
2017-01-31 02:01:56 +0100

    test: Fix tests with done.fail

--- server/__tests__/api.test.js
+++ server/__tests__/api.test.js
@@ -4,28 +4,29 @@ import request from 'supertest'
 
 describe('API', () => {
   describe('GET /api/health-check', () =>
-      it('should return OK', done =>
-        request(app)
-          .get('/api/health-check')
-          .expect(httpStatus.OK)
-          .then(res => {
-            expect(res.text).to.equal('OK')
-            done()
-          })
-          .catch(done)
-      )
+    it('should return OK', done =>
+      request(app)
+        .get('/api/health-check')
+        .expect(httpStatus.OK)
+        .then(res => {
+          expect(res.text).toEqual('OK')
+          done()
+        })
+        .catch(done.fail)
+    )
   )
 
   describe('GET /api/404', () =>
     it('should return a 404 error', done => {
       request(app)
-        .get('/api/boards')
+        .get('/api/404')
         .expect(httpStatus.NOT_FOUND)
         .then(res => {
-          expect(res.body.message).to.equal('Not Found')
+          console.log(res)
+          expect(res.body.message).toEqual('Not Found')
           done()
         })
-        .catch(done)
+        .catch(done.fail)
     })
   )
 })
--- server/__tests__/board.test.js
+++ server/__tests__/board.test.js
@@ -1,18 +1,45 @@
 import app from '../index'
 import httpStatus from 'http-status'
+import mongoose from 'mongoose'
 import request from 'supertest'
 
-describe('Board APIs', () =>
+afterAll(done => {
+  mongoose.models = {}
+  mongoose.modelSchemas = {}
+  mongoose.connection.db.dropDatabase()
+  mongoose.connection.close()
+  done()
+})
+
+const board = {
+  title: 'Cartomancy Board'
+}
+
+describe('Board APIs', () => {
   describe('GET /api/boards', () =>
     it('should return an empty list', done => {
       request(app)
         .get('/api/boards')
         .expect(httpStatus.OK)
         .then(res => {
-          expect(res.body.username).to.equal([])
+          expect(res.body).toEqual([])
           done()
         })
-        .catch(done)
+        .catch(done.fail)
     })
   )
-)
+
+  describe('POST /api/boards', () =>
+    it('should create a new board', done => {
+      request(app)
+        .post('/api/boards')
+        .send(board)
+        .expect(httpStatus.OK)
+        .then(res => {
+          expect(res.body.title).toEqual(board.title)
+          done()
+        })
+        .catch(done.fail)
+    })
+  )
+})
